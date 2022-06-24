74b9fa1d020a77e405f3fb7ed210ce88c60c0043
Daniel Mateos <daniel@mateos.cc>
2022-06-25 03:51:59 +1000

    more messing around

---
+++ 2dtiletest/Assets/scripts/Bee.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bee : MonoBehaviour {
+    void Start() {
+        
+    }
+
+    void Update() {
+        
+    }
+
+    void OnTriggerEnter(Collider c) {
+        Debug.Log("hit!");
+    }
+}
