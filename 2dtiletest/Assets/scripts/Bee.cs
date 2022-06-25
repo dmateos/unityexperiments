32ff86d3ae28748f88056c010660bb7a26917289
Daniel Mateos <daniel@mateos.cc>
2022-06-26 01:31:15 +1000

    bee logic

--- 2dtiletest/Assets/scripts/Bee.cs
+++ 2dtiletest/Assets/scripts/Bee.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Bee : MonoBehaviour {
+    private Rigidbody2D rb;
+
     void Start() {
-        
+        rb = GetComponent<Rigidbody2D>(); 
     }
 
     void Update() {
         
     }
 
-    void OnTriggerEnter(Collider c) {
+    void OnCollisionEnter2D(Collision2D c) {
         Debug.Log("hit!");
+        rb.gravityScale = 1.0f;
     }
 }
