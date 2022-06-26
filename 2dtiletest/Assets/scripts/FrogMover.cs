c3b67fd36f315a69009bdab8705bf2774af3982b
Daniel Mateos <daniel@mateos.cc>
2022-06-26 20:20:08 +1000

    vscode

--- 2dtiletest/Assets/scripts/FrogMover.cs
+++ 2dtiletest/Assets/scripts/FrogMover.cs
@@ -12,10 +12,11 @@ public class FrogMover : MonoBehaviour {
     public GameObject bullet;
 
     void Start() {
-        rb = GetComponent<Rigidbody2D>(); 
+        rb = GetComponent<Rigidbody2D>();
         sr = GetComponent<SpriteRenderer>();
         animator = GetComponent<Animator>();
     }
+
 
     void Update() {
         var mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
@@ -23,20 +24,20 @@ public class FrogMover : MonoBehaviour {
         bool fire = Input.GetMouseButtonDown(0);
         bool jump = Input.GetKeyDown("space");
 
-        if(horizontal == -1) {
+        if (horizontal == -1) {
             sr.flipX = false;
-        } else if(horizontal == 1){
+        } else if (horizontal == 1) {
             sr.flipX = true;
         }
 
-        if(jump) {
+        if (jump) {
             transform.position += new Vector3(0.0f, 1.0f, 0.0f);
             animator.Play("Base Layer.FrogAnimation", 0, 0.0f);
         }
         rb.velocity = new Vector2(horizontal * movement_speed, rb.velocity.y);
 
-        if(fire) {
-             Instantiate(bullet, transform.position, Quaternion.identity);
+        if (fire) {
+            Instantiate(bullet, transform.position, Quaternion.identity);
         }
     }
 }
