9786fef1b88f4b8ada34ca216a9759ae957fb63e
Daniel Mateos <daniel@mateos.cc>
2022-07-01 00:42:31 +1000

    start of conway implementation

--- 3dPathTest/Assets/GameOfLife.cs
+++ 3dPathTest/Assets/GameOfLife.cs
@@ -3,34 +3,57 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameOfLife : MonoBehaviour {
-    private int[,] WorldMap;
-    private GameObject[,] CubeMap;
     public GameObject Cube;
-
-    private const int cubes = 100;
-    private const int cube_space = 1;
+    private int[,] _stateMap;
+    private GameObject[,] _cubeMap;
+    private const int _cubeCount = 100;
+    private const int _cubeSpacing = 1;
 
     void Start() {
-        WorldMap = new int[cubes, cubes];
-        CubeMap = new GameObject[cubes, cubes];
+        _stateMap = new int[_cubeCount, _cubeCount];
+        _cubeMap = new GameObject[_cubeCount, _cubeCount];
 
-        for (int i = 0; i < cubes; i++) {
-            for (int y = 0; y < cubes; y++) {
-                WorldMap[i, y] = Random.Range(0, 2);
-                CubeMap[i, y] = Instantiate(Cube, new Vector3(i * cube_space, y * cube_space, 0), Quaternion.identity);
+        for (int i = 0; i < _cubeCount; i++) {
+            for (int y = 0; y < _cubeCount; y++) {
+                _stateMap[i, y] = Random.Range(0, 2);
+                _cubeMap[i, y] = Instantiate(Cube, new Vector3(i * _cubeSpacing, y * _cubeSpacing, 0), Quaternion.identity);
             }
         }
     }
 
     void Update() {
-        for (int i = 0; i < cubes; i++) {
-            for (int y = 0; y < cubes; y++) {
-                if (WorldMap[i, y] == 0) {
-                    CubeMap[i, y].SetActive(false);
+        ComputeLife();
+
+        for (int i = 0; i < _cubeCount; i++) {
+            for (int y = 0; y < _cubeCount; y++) {
+                if (_stateMap[i, y] == 0) {
+                    _cubeMap[i, y].SetActive(false);
                 } else {
-                    CubeMap[i, y].SetActive(true);
+                    _cubeMap[i, y].SetActive(true);
                 }
             }
         }
+    }
+
+    void ComputeLife() {
+        for (int i = 0; i < _cubeCount; i++) {
+            for (int y = 0; y < _cubeCount; y++) {
+                _stateMap[i, y] = Random.Range(0, 2);
+            }
+        }
+    }
+
+    int GetNeighbourState(int x, int y, string direction) {
+        switch (direction) {
+            case "north":
+                return _stateMap[x, y--];
+            case "east":
+                return _stateMap[x++, y];
+            case "south":
+                return _stateMap[x, y++];
+            case "west":
+                return _stateMap[x--, y];
+        }
+        return 0;
     }
 }
